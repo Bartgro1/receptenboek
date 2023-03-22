bdbc2e7fd5b922015844797251e7e3e359e13428
Bart Groothuysen <bart@groothuysen.com>
2023-03-22 11:25:59 +0100

    structuur index.php: 2

--- site/index.php
+++ site/index.php
@@ -7,30 +7,47 @@ require 'database.php';
 ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="css/style1.css">
-    <link rel="stylesheet" href="css/style2.css">
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="css/style1.css">
+  <link rel="stylesheet" href="css/style2.css">
 </head>
+
 <body>
-    <?php include 'header.php'?>
-    <?php include 'nav.php'?>
-    <main>
-      <div class="container">
-        <div class="flex-container">
-           <div class="recepten">
-             <h1> Asado op de grill </h1>
-             <p> Serves 1 - prep Time: 5 mins - ready in 2 mins</p>
-                <hr>
-                <img src="images/asado-short-ribs.png" alt="">
-             </div>
-           </div>
+  <?php include 'header.php' ?>
+  <?php include 'nav.php' ?>
+  <main>
+    <div class="container">
+      <div class="flex-container">
+        <div class="recepten">
+          <div class="recept-tijd-titel">
+            <h1> Asado op de grill </h1>
+            <p> Serves 1 - prep Time: 5 mins - ready in 2 mins</p>
+            <hr>
+          </div>
+          <div class="recept-box">
+            <div class="recept-ing">
+              <img src="images/asado-short-ribs.png" alt="">
+               <p> ingreidne: </p>
+               
+            </div>
+               <div class="recept-instr">
+             <h2> instrucies</h2>
+             <p> 1. ga naar de keuken en pak een hamer
+                2. stap in de douche
+              3. dans </p>
+          </div>
+          </div>
         </div>
       </div>
-    </main>
-    <?php include 'footer.php'?>
+    </div>
+    </div>
+  </main>
+  <?php include 'footer.php' ?>
 </body>
+
 </html>
