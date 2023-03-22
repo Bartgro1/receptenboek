e3c8d61727c4269e41b3844063cc4bbd867b7d83
Bart Groothuysen <bart@groothuysen.com>
2023-03-22 09:42:43 +0100

    wijzigen gemaakt betreft bestanden php

--- site/database.php
+++ site/database.php
@@ -9,12 +9,3 @@ $dbname = "receptenboek";
 $conn = mysqli_connect($host, $dbuser, $dbpass, $dbname);
 
 
-// Controleer de verbinding
-if(mysqli_connect_error())
-{
- echo "Connection establishing failed!";
-}
-else
-{
- echo "Connection established successfully.";
-}
--- site/index.php
+++ site/index.php
@@ -12,13 +12,15 @@ require 'database.php';
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="style.css">
 </head>
 <body>
-
+    
     <main>
       <div class="container">
-        
+         <p>adsdasdsadsa</p>
       </div>
     </main>
+    <?php include 'footer.php'?>
 </body>
 </html>
