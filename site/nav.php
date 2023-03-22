4dd9329ed523a93ff84e8f5e9b1f770a61d68e1a
Bart Groothuysen <bart@groothuysen.com>
2023-03-22 10:26:46 +0100

    li's toegevoegd navigatie

--- site/nav.php
+++ site/nav.php
@@ -14,7 +14,9 @@
         <div class="container">
             <ul>
                 <li> <a href="index.php">Home</a></li>
-             
+                <li> <a href="#">Recepten</a></li>
+                <li> <a href="#">Recept</a></li>
+                <li> <a href="#">Specials</a></li>
             </ul>
         </div>
     </nav>
