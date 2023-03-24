3f0c4d440e1da2f794ad97ab14f60df4ba854602
Bart Groothuysen <bart@groothuysen.com>
2023-03-24 12:46:26 +0100

    overlay gemaakt voor recept image

--- site/recepten.php
+++ site/recepten.php
@@ -19,7 +19,7 @@ $recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <link rel="stylesheet" href="css/style1.css">
-  <link rel="stylesheet" href="css/style3.css">
+  <link rel="stylesheet" href="css/style2.css">
 </head>
 
 <body>
@@ -30,54 +30,41 @@ $recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
       <div class="flex-container">
         <?php foreach ($recepten as $recept) : ?>
           <div class="recepten">
-            <div class="recept-tijd-titel">
-              <h1> <?php echo $recept['naam'] ?> </h1>             
-              <hr>
-            </div>
-            <div class="recept-box">
-              <div class="recept-algemeen">
-                <img class="recept-foto" src="<?php echo  $recept['plaatje'] ?>">
-             .
-                <div class="timers">             
-                <div class="timer-tijd">
-                  <img class="timer" src="images/klok.png" alt="">
-                  <h2>bereidingstijd</h2>
-                </div>
-                <div class="bereidingstijd">
-                <p class="kooktijd"> <?php echo $recept['bereidingstijd'] ?> </p>
-                </div>
-                </div>
+            <div class="blok">
+              <img src="<?php echo  $recept['plaatje'] ?>">
+              <div class="tekst-overlay">
+              <div class="overlay-bar">
+                <div class="overlay-bar-container">
+                  <div class="overlay-bar-items">
+                    <img src="images/gerecht.png" alt="">
+                    <p> <?php echo $recept['menugang'] ?></p>
+                  </div>
+              </div>
+        <!--      <div class="overlay-bar">
+                <div class="overlay-bar-container">
+                  <div class="overlay-bar-items">
+                    <img src="images/gerecht.png" alt="">
+                    <p> <?php echo $recept['menugang'] ?></p>
+                  </div>
+                  <div class="overlay-bar-items">
+                         <img src="images/klok.png" alt="">
+                    <p> <?php echo $recept['bereidingstijd'] ?></p>
+                  </div>
+                  <div class="overlay-bar-items">
+                  <img src="images/ster.png" alt="">
+                    <p> <?php echo $recept['moeilijkheidsgraad'] ?></p>
 
-                <div class="timers">
-                <div class="timer-tijd">
-                  <img class="timer" src="images/klok.png" alt="">
-                  <h2>kooktijd</h2>
                   </div>
-                  <div class="bereidingstijd">
-                  <p class="kooktijd">  <?php echo $recept['kooktijd'] ?> </p>
-                  <div class="ingrediënten">
-                      <h2> ingrediënten </h2>
-                      <p><?php echo $recept['ingredienten'] ?></p>
-                  </div>
-                  </div>            
-                </div>
-                 </div>
-              
-
-              <div class="recept-info">
-                <h2> <?php echo $recept['menugang'] ?></h2>
-                <p> <?php echo $recept['bereidingswijze'] ?> </p>
-                <div class="blok"> 
-                  <div class="blok-inhoud">
-                   <img src="images/ster.png" alt=""> 
-                  <p> <?php echo $recept['moeilijkheidsgraad']?></p>
+                  <div class="overlay-bar-items">
+                    <img src="images/persoon.png" alt="">
+                    <p> <?php echo $recept['aantal personen'] ?></p> -->
                   </div>
                 </div>
-                
               </div>
             </div>
-            <hr>
-          <?php endforeach; ?>
+          </div>
+      </div>
+    <?php endforeach; ?>
     </div>
     </div>
   </main>
