0e73327265bb8961c847b37a381d57987d236df7
Bart Groothuysen <bart@groothuysen.com>
2023-03-24 08:46:23 +0100

    html toegevoegd & php aan recepten.php

---
+++ site/recepten.php
@@ -0,0 +1,29 @@
+<?php
+
+
+require 'database.php';
+
+$sql = "SELECT * FROM argentijnse_keuken";
+
+$result = mysqli_query($conn, $sql);
+
+$alle_recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="stylesheet" href="css/style2.css">
+</head>
+<body>
+    
+</body>
+</html>
