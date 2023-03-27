52f46c116720333a3e9b73a6cea220aa185788c5
Bart Groothuysen <bart@groothuysen.com>
2023-03-27 13:54:56 +0200

    wijziging gemaakt

--- site/header.php
+++ site/header.php
@@ -1,3 +1,23 @@
+<!--<?php
+
+require 'database.php';
+
+
+
+$sql = "SELECT count(naam) AS aantal FROM argentijnse_keuken ";
+
+$result  = mysqli_query($conn, $sql);
+
+$recept = mysqli_fetch_assoc($result);
+
+var_dump($recept);
+
+
+
+
+?>-->
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,8 +27,10 @@
     <link rel="stylesheet" href="css/style1.css">
 </head>
 <body>
-    <header>
-             <img src="images/header_background.png" alt="">               
+    <header class="top-header">
+          <span style="color:goldenrod; font-size:30px" ><?php echo $recept['aantal'] ?></span>
+                   
     </header>
+
 </body>
 </html>
