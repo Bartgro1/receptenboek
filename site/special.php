dd14cce3c33f7a66e7bda9a77afb2dd3e1ab7f37
Bart Groothuysen <bart@groothuysen.com>
2023-03-29 21:01:57 +0200

    testen gedaan special.php

--- site/special.php
+++ site/special.php
@@ -1,6 +1,7 @@
 <?php
 
 
+
 require 'database.php';
 
 $sql = "SELECT * FROM argentijnse_keuken";
@@ -9,62 +10,77 @@ $result = mysqli_query($conn, $sql);
 
 $recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
+
+
+
+$sql2 = "SELECT * FROM argentijnse_keuken where moeilijkheidsgraad = 'makkelijk'";
+
+
+$result  = mysqli_query($conn, $sql2);
+
+$makkelijk = mysqli_fetch_assoc($result);
+
+
 ?>
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
-  <link rel="stylesheet" href="css/style1.css">
-  <link rel="stylesheet" href="css/style2.css">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css/style1.css">
+    <link rel="stylesheet" href="css/style2.css">
 </head>
 
 <body>
-  <?php include 'header.php' ?>
-  <?php include 'nav.php' ?>
-  <main>
-    <div class="container">
-      <div class="flex-container">
-        <?php foreach ($recepten as $recept) : ?>
+    <?php include 'header.php' ?>
+    <?php include 'nav.php' ?>
+    <main>
+        <div class="container">
+            <div class="flex-container">
+                <div class="buttons">
 
-          <div class="recept">
-            <a href="recept.php?id=<?php echo $recept['recepten_id'] ?>">
-              <img src="<?php echo  $recept['plaatje'] ?>">
-              <div class="receptnaam">
-                <h2> <?php echo $recept['naam'] ?></h2>
-              </div>
-              <div class="overlay">
-                <div class="overlay-bar">
-                  <div class="overlay-bar-container">
-                    <div class="overlay-bar-items">
-                      <img src="images/gerecht.png" alt="">
-                      <p> <?php echo $recept['menugang'] ?></p>
-                    </div>
-                    <div class="overlay-bar-items">
-                      <img src="images/klok.png" alt="">
-                      <p> <?php echo $recept['totale tijd'] ?></p>
-                    </div>
-                    <div class="overlay-bar-items">
-                      <img src="images/ster.png" alt="">
-                      <p> <?php echo $recept['moeilijkheidsgraad'] ?></p>
-                    </div>
-                    <div class="overlay-bar-items">
-                      <img src="images/persoon.png" alt="">
-                      <p> <?php echo $recept['aantal personen'] ?></p>
-                    </div>
-                  </div>
+
+
+                    <form action="special.php">
+                        <button type="submit">Click me </button>
+                    </form>
+
+
                 </div>
-            </a>
-          </div>
-      </div>
-<?php endforeach; ?>
-    
+                <?php foreach ($recepten as $recept) : ?>
+                    <div class="recept">
+                        <div class="receptnaam">
 
-  </main>
-  <?php include 'footer.php' ?>
+                        </div>
+                        <div class="overlay">
+                            <div class="overlay-bar">
+                                <div class="overlay-bar-container">
+                                    <div class="overlay-bar-items">
+                                        <img src="images/gerecht.png" alt="">
+
+                                    </div>
+                                    <div class="overlay-bar-items">
+                                        <img src="images/klok.png" alt="">
+
+                                    </div>
+                                    <div class="overlay-bar-items">
+                                        <img src="images/ster.png" alt="">
+
+                                    </div>
+                                    <div class="overlay-bar-items">
+                                        <img src="images/persoon.png" alt="">
+                                    </div>
+                                </div>
+                            </div>
+                            </a>
+                        </div>
+                    </div>
+                <?php endforeach; ?>
+    </main>
+    <?php include 'footer.php' ?>
 </body>
 
 </html>
