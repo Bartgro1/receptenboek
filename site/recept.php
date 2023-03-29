b237f5ee7eb2de2040b8302f32997c653bd33e35
Bart Groothuysen <bart@groothuysen.com>
2023-03-29 21:47:43 +0200

    div aangemaakt voor buttons

--- site/recept.php
+++ site/recept.php
@@ -29,66 +29,64 @@ $recept = mysqli_fetch_assoc($result);
   <main>
     <div class="container">
       <div class="flex-container">
-          <div class="recepten">
-            <div class="recept-tijd-titel">
-              <h1> <?php echo $recept['naam'] ?> </h1>             
-              <hr>
-            </div>
-            <div class="recept-box">
-              <div class="recept-algemeen">
-                <img class="recept-foto" src="<?php echo  $recept['plaatje'] ?>">
-             .
-                <div class="timers">             
+        <div class="recepten">
+          <div class="recept-tijd-titel">
+            <h1> <?php echo $recept['naam'] ?> </h1>
+            <hr>
+          </div>
+          <div class="recept-box">
+            <div class="recept-algemeen">
+              <img class="recept-foto" src="<?php echo  $recept['plaatje'] ?>">
+              .
+              <div class="timers">
                 <div class="timer-tijd">
                   <img class="timer" src="images/klok.png" alt="">
                   <h2>bereidingstijd</h2>
                 </div>
                 <div class="bereidingstijd">
-                <p class="kooktijd"> <?php echo $recept['bereidingstijd'] ?> </p>
+                  <p class="kooktijd"> <?php echo $recept['bereidingstijd'] ?> </p>
                 </div>
-                </div>
+              </div>
 
-                <div class="timers">             
+              <div class="timers">
                 <div class="timer-tijd">
                   <img class="timer" src="images/klok.png" alt="">
                   <h2>kooktijd</h2>
                 </div>
                 <div class="bereidingstijd">
-                <p class="kooktijd"> <?php echo $recept['kooktijd'] ?> </p>
+                  <p class="kooktijd"> <?php echo $recept['kooktijd'] ?> </p>
                 </div>
-                </div>
+              </div>
 
-                <div class="timers">
+              <div class="timers">
                 <div class="timer-tijd">
                   <img class="timer" src="images/klok.png" alt="">
                   <h2>Totale bereidingstijd</h2>
-                  </div>
-                  <div class="bereidingstijd">
-                  <p class="kooktijd">  <?php echo $recept['totale tijd'] ?> </p>
+                </div>
+                <div class="bereidingstijd">
+                  <p class="kooktijd"> <?php echo $recept['totale tijd'] ?> </p>
                   <div class="ingrediënten">
-                      <h2> ingrediënten </h2>
-                      <p><?php echo $recept['ingredienten'] ?></p>
-                  </div>
-                  </div>            
-                </div>
-                 </div>
-              
-
-              <div class="recept-info">
-                <h2> <?php echo $recept['menugang'] ?></h2>
-                <p> <?php echo $recept['bereidingswijze'] ?> </p>
-                <div class="blok"> 
-                  <div class="blok-inhoud">
-                   <img src="images/ster.png" alt=""> 
-                  <p> <?php echo $recept['moeilijkheidsgraad']?></p>
+                    <h2> ingrediënten </h2>
+                    <p><?php echo $recept['ingredienten'] ?></p>
                   </div>
                 </div>
-                
               </div>
             </div>
-            <hr>
-    </div>
-    </div>
+
+            <div class="recept-info">
+              <h2> <?php echo $recept['menugang'] ?></h2>
+              <p> <?php echo $recept['bereidingswijze'] ?> </p>
+              <div class="blok">
+                <div class="blok-inhoud">
+                  <img src="images/ster.png" alt="">
+                  <p> <?php echo $recept['moeilijkheidsgraad'] ?></p>
+                </div>
+              </div>
+            </div>
+          </div>
+          <hr>
+        </div>
+      </div>
   </main>
   <?php include 'footer.php' ?>
 </body>
--- site/special.php
+++ site/special.php
@@ -38,47 +38,45 @@ $makkelijk = mysqli_fetch_assoc($result);
     <?php include 'header.php' ?>
     <?php include 'nav.php' ?>
     <main>
-        <div class="container">
-            <div class="flex-container">
-                <div class="buttons">
+    <div class="container">
+        <div class="buttons">
+         
+        </div>
+      <div class="gerechten-container">       
+        <?php foreach ($recepten as $recept) : ?>
 
-
-
-                    <form action="special.php">
-                        <button type="submit">Click me </button>
-                    </form>
-
-
-                </div>
-                <?php foreach ($recepten as $recept) : ?>
-                    <div class="recept">
-                        <div class="receptnaam">
-
-                        </div>
-                        <div class="overlay">
-                            <div class="overlay-bar">
-                                <div class="overlay-bar-container">
-                                    <div class="overlay-bar-items">
-                                        <img src="images/gerecht.png" alt="">
-
-                                    </div>
-                                    <div class="overlay-bar-items">
-                                        <img src="images/klok.png" alt="">
-
-                                    </div>
-                                    <div class="overlay-bar-items">
-                                        <img src="images/ster.png" alt="">
-
-                                    </div>
-                                    <div class="overlay-bar-items">
-                                        <img src="images/persoon.png" alt="">
-                                    </div>
-                                </div>
-                            </div>
-                            </a>
-                        </div>
+          <div class="gerechten">
+            
+            <a href="recept.php?id=<?php echo $recept['recepten_id'] ?>">
+              <img src="<?php echo  $recept['plaatje'] ?>">
+              <div class="gerechten-naam">
+                <h2> <?php echo $recept['naam'] ?></h2>
+              </div>
+              <div class="overlay">
+                <div class="overlay-bar">
+                  <div class="overlay-bar-container">
+                    <div class="overlay-bar-items">
+                      <img src="images/gerecht.png" alt="">
+                      <p> <?php echo $recept['menugang'] ?></p>
                     </div>
-                <?php endforeach; ?>
+                    <div class="overlay-bar-items">
+                      <img src="images/klok.png" alt="">
+                      <p> <?php echo $recept['totale tijd'] ?></p>
+                    </div>
+                    <div class="overlay-bar-items">
+                      <img src="images/ster.png" alt="">
+                      <p> <?php echo $recept['moeilijkheidsgraad'] ?></p>
+                    </div>
+                    <div class="overlay-bar-items">
+                      <img src="images/persoon.png" alt="">
+                      <p> <?php echo $recept['aantal personen'] ?></p>
+                    </div>
+                  </div>
+                </div>
+            </a>
+          </div>
+      </div>
+<?php endforeach; ?>
     </main>
     <?php include 'footer.php' ?>
 </body>
