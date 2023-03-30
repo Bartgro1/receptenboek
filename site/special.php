1c6bcd7d5dde1ff76718f7cc471a13a7e36763c7
Bart Groothuysen <bart@groothuysen.com>
2023-03-30 10:35:13 +0200

    new div class aangemaakt en sql queries opgeslagen

--- site/special.php
+++ site/special.php
@@ -4,21 +4,31 @@
 
 require 'database.php';
 
-$sql = "SELECT * FROM argentijnse_keuken";
 
-$result = mysqli_query($conn, $sql);
+
+
+
+
+$sql = "SELECT * FROM argentijnse_keuken where moeilijkheidsgraad = 'makkelijk'";
+
+$result  = mysqli_query($conn, $sql);
 
 $recepten = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
 
-
-
-$sql2 = "SELECT * FROM argentijnse_keuken where moeilijkheidsgraad = 'makkelijk'";
-
+$sql2 = "SELECT * FROM argentijnse_keuken where kooktijd = (select max(kooktijd) FROM argentijnse_keuken)";
 
 $result  = mysqli_query($conn, $sql2);
 
-$makkelijk = mysqli_fetch_assoc($result);
+$kooktijden = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
+
+$sql3 = "SELECT * FROM argentijnse_keuken where ingredienten = (select max(ingredienten) FROM argentijnse_keuken)";
+
+$result  = mysqli_query($conn, $sql3);
+
+$totale_ingredienten = mysqli_fetch_all($result, MYSQLI_ASSOC);
+
 
 
 ?>
@@ -39,17 +49,10 @@ $makkelijk = mysqli_fetch_assoc($result);
     <?php include 'nav.php' ?>
     <main>
     <div class="container">
-        <div class="buttons">
-        <div class="button">
-                <button></button>
-            </div>
-            <div class="button">
-                <button></button>
-            </div>
-            <div class="button">
-                <button></button>
-            </div>
-        </div>
+      <div class="test">
+            <h2> kooktijd  </h2>
+      </div>
+
       <div class="gerechten-container">       
         <?php foreach ($recepten as $recept) : ?>
 
@@ -81,10 +84,81 @@ $makkelijk = mysqli_fetch_assoc($result);
                     </div>
                   </div>
                 </div>
-            </a>
+            </a>        
           </div>
-      </div>
-<?php endforeach; ?>
+      </div> 
+    <?php endforeach; ?>
+    
+    <div class="gerechten-container">       
+        <?php foreach ($kooktijden as $kooktijd) : ?>
+            
+          <div class="gerechten">
+            
+            <a href="recept.php?id=<?php echo $kooktijd['recepten_id'] ?>">
+              <img src="<?php echo  $kooktijd['plaatje'] ?>">
+              <div class="gerechten-naam">
+                <h2> <?php echo $kooktijd['naam'] ?></h2>
+              </div>
+              <div class="overlay">
+                <div class="overlay-bar">
+                  <div class="overlay-bar-container">
+                    <div class="overlay-bar-items">
+                      <img src="images/gerecht.png" alt="">
+                      <p> <?php echo $kooktijd['menugang'] ?></p>
+                    </div>
+                    <div class="overlay-bar-items">
+                      <img src="images/klok.png" alt="">
+                      <p> <?php echo $kooktijd['totale tijd'] ?></p>
+                    </div>
+                    <div class="overlay-bar-items">
+                      <img src="images/ster.png" alt="">
+                      <p> <?php echo $kooktijd['moeilijkheidsgraad'] ?></p>
+                    </div>
+                    <div class="overlay-bar-items">
+                      <img src="images/persoon.png" alt="">
+                      <p> <?php echo $kooktijd['aantal personen'] ?></p>
+                    </div>
+                  </div>
+                </div>
+            </a>        
+          </div>
+      </div> 
+      <?php endforeach; ?>
+      <div class="gerechten-container">       
+        <?php foreach ($totale_ingredienten as $ingrediënt) : ?>
+
+          <div class="gerechten">
+            
+            <a href="recept.php?id=<?php echo $ingrediënt['recepten_id'] ?>">
+              <img src="<?php echo  $ingrediënt['plaatje'] ?>">
+              <div class="gerechten-naam">
+                <h2> <?php echo $ingrediënt['naam'] ?></h2>
+              </div>
+              <div class="overlay">
+                <div class="overlay-bar">
+                  <div class="overlay-bar-container">
+                    <div class="overlay-bar-items">
+                      <img src="images/gerecht.png" alt="">
+                      <p> <?php echo $ingrediënt['menugang'] ?></p>
+                    </div>
+                    <div class="overlay-bar-items">
+                      <img src="images/klok.png" alt="">
+                      <p> <?php echo $ingrediënt['totale tijd'] ?></p>
+                    </div>
+                    <div class="overlay-bar-items">
+                      <img src="images/ster.png" alt="">
+                      <p> <?php echo $ingrediënt['moeilijkheidsgraad'] ?></p>
+                    </div>
+                    <div class="overlay-bar-items">
+                      <img src="images/persoon.png" alt="">
+                      <p> <?php echo $ingrediënt['aantal personen'] ?></p>
+                    </div>
+                  </div>
+                </div>
+            </a>        
+          </div>
+      </div> 
+      <?php endforeach; ?>
     </main>
     <?php include 'footer.php' ?>
 </body>
