3c4ebaafa09c0741f49e8c36285f55ddb0a3d627
Bart Groothuysen <bart@groothuysen.com>
2023-05-22 09:14:53 +0200

    verzeker wachtwoord weggehaald

--- site/verwerk-registratie.php
+++ site/verwerk-registratie.php
@@ -2,23 +2,22 @@
 
 
 
-    
-    
+   
         $voornaam              = $_POST['voornaam'];
         $achternaam            = $_POST['achternaam'];
         $email                 = $_POST['email'];
         $telefoonnummer        = $_POST['telefoonnummer'];
         $wachtwoord             = $_POST['wachtwoord'];
-        $verzeker_wachtwoord   = $_POST['verzeker_wachtwoord'];
       
         require 'database.php';
     
         // wachtwoord hashen
-        $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+        $hashed_password = password_hash($wachtwoord, PASSWORD_DEFAULT);
+        
 
 
-        $sql =  "INSERT INTO gebruikers(voornaam, achternaam, email, telefoonnummer, wachtwoord, verzeker_wachtwoord) 
-        VALUES ('$voornaam','$achternaam','$email','$telefoonnummer','$wachtwoord','$verzeker_wachtwoord')";
+        $sql =  "INSERT INTO gebruikers(voornaam, achternaam, email, telefoonnummer, wachtwoord) 
+        VALUES ('$voornaam','$achternaam','$email','$telefoonnummer','$hashed_password')";
     
         if (mysqli_query($conn, $sql)) {
             header("location: inloggen.php");
